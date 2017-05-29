d1a2b013f521e910acc3e9c86f4e95e32b944920
Joshua Glover <joshua.glover@klarna.com>
2017-05-29 17:33:53 +0200

    Trivial API

--- src/gentest_playground/api.clj
+++ src/gentest_playground/api.clj
@@ -1,22 +1,44 @@
 (ns gentest-playground.api
   (:require [com.stuartsierra.component :as component]
             [gentest-playground.apple :as apple]
-            [schema.core :refer [Keyword]]
+            [schema.core :refer [Uuid]]
             [yada
              [resource :refer [resource]]
              [yada :as yada]]))
 
-(def apples (atom {}))
+(defonce apples (atom {}))
 
 (def routes
   ["/apples"
-   [["" (resource
-         {:description "All the apples"
-          :produces [{:media-type #{"application/json;q=0.8"}
-                      :charset "UTF-8"}]
-          :methods
-          {:get {:response (fn [ctx]
-                             (map apple/->response (apple/sort (vals @apples))))}}})]]])
+   [[""
+     (resource
+      {:id ::index
+       :description "All the apples"
+       :produces [{:media-type #{"application/json;q=0.8"}
+                   :charset "UTF-8"}]
+       :methods
+       {:get {:response (fn [ctx]
+                          (map apple/->response (apple/sort (vals @apples))))}
+        :post {:parameters {:body {:colour String
+                                   :grams Integer
+                                   :variety String}}
+               :consumes [{:media-type #{"application/json"}
+                           :charset "UTF-8"}]
+               :response (fn [ctx]
+                           (let [{:keys [:apple/id] :as apple} (apple/create (get-in ctx [:parameters :body]))]
+                             (swap! apples assoc id apple)
+                             (java.net.URI. (str "http://localhost:8842/apples/" id))))}}})]
+    [["/" :apple]
+     (resource
+      {:id ::apple
+       :description "An apple"
+       :parameters {:path {:apple Uuid}}
+       :produces [{:media-type #{"application/json;q=0.8"}
+                   :charset "UTF-8"}]
+       :methods
+       {:get {:response (fn [ctx]
+                          (let [id (get-in ctx [:parameters :path :apple])]
+                            (apple/->response (get @apples id))))}}})]]])
 
 (defrecord Api [config]
   component/Lifecycle
--- src/gentest_playground/apple.clj
+++ src/gentest_playground/apple.clj
@@ -1,5 +1,13 @@
 (ns gentest-playground.apple
-  (:refer-clojure :exclude [sort]))
+  (:refer-clojure :exclude [sort])
+  (:import (java.util UUID)))
+
+(defn create [{:keys [colour grams variety]}]
+  {:apple/id (UUID/randomUUID)
+   :apple/colour colour
+   :apple/variety variety
+   :apple/weight {:value grams
+                  :unit :grams}})
 
 (defn sort [apples]
   apples)
