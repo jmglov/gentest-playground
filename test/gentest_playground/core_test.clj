901da6b64f227bc930da01d9eeaec52058e3e521
Joshua Glover <joshua.glover@klarna.com>
2017-05-30 13:56:26 +0200

    Apple tests

--- test/gentest_playground/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns gentest-playground.core-test
-  (:require [clojure.test :refer :all]
-            [gentest-playground.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
